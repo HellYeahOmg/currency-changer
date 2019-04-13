b3812e66f218e3663a0571afeec56c9575a5c76b
Vadim <vadim@iMac-Vadim.Dlink>
2019-04-13 16:21:15 +0300

    Updates

---
+++ precache-manifest.15bdf005824cf8b82a4a1a49dc4803bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f40188c78b1f0a9aa60b",
+    "url": "/currency-changer/static/js/runtime~main.3f0e4cec.js"
+  },
+  {
+    "revision": "a25ee14ea4781b9a9f04",
+    "url": "/currency-changer/static/js/main.33d8ebba.chunk.js"
+  },
+  {
+    "revision": "12210daf814efa77c0a3",
+    "url": "/currency-changer/static/js/2.f212b9dc.chunk.js"
+  },
+  {
+    "revision": "a25ee14ea4781b9a9f04",
+    "url": "/currency-changer/static/css/main.a2c86c10.chunk.css"
+  },
+  {
+    "revision": "a064f06378f19c0678fc15c8353676d1",
+    "url": "/currency-changer/index.html"
+  }
+];
